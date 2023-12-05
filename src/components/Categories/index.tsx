a021921108b8620078ca70c6a7ba71c7376a4222
Pedro Gomes <pedro-gomes@ibm.com>
2023-12-05 18:04:10 -0300

    feat: create categories component list

---
+++ src/components/Categories/index.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { FlatList } from 'react-native'
+import { CarCategory } from '../Cards/CarCategory'
+import { styles } from './categories.styles'
+
+type ItemData = {
+  id: string
+  title: string
+}
+
+const DATA: ItemData[] = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'BMW',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'BMW',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'BMW',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d76',
+    title: 'BMW',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d744',
+    title: 'BMW',
+  },
+]
+
+export const Categories = () => {
+  const [selectedId, setSelectedId] = useState<string>()
+
+  return (
+    <FlatList
+      data={DATA}
+      style={styles.list}
+      renderItem={({ item }: { item: ItemData }) => {
+        return (
+          <CarCategory
+            title={item.title}
+            onPress={() => setSelectedId(item.id)}
+            isSelected={item.id === selectedId}
+          />
+        )
+      }}
+      contentContainerStyle={styles.itens}
+      keyExtractor={(item) => item.id}
+      extraData={selectedId}
+      horizontal
+    />
+  )
+}
