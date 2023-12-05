38ba074d58d3e20e733dff90a9c761956be40f2c
Pedro Gomes <pedro-gomes@ibm.com>
2023-12-05 18:03:21 -0300

    style: adjust header styles

--- src/components/header/header.styles.ts
+++ src/components/header/header.styles.ts
@@ -5,15 +5,11 @@ export const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginTop: 60,
-    marginRight: 20,
-    marginLeft: 20,
+    marginHorizontal: 20,
   },
   containerProfile: {
     flexDirection: 'column',
     marginLeft: 20,
-  },
-  text: {
-    color: '#fff',
   },
   username: {
     fontSize: 16,
