f8029c45b11f56f2e52228aa1ac0b14ff6625876
Pedro Gomes <pedro-gomes@ibm.com>
2023-12-05 18:05:14 -0300

    feat: create car category card component

---
+++ src/components/Cards/CarCategory/car-category.styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  item: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 5,
+    width: 88,
+    height: 88,
+    borderColor: '#d8d8d833',
+    borderWidth: 1,
+    borderRadius: 10,
+    marginTop: 20,
+    backgroundColor: '#d8d8d80d',
+  },
+  image: {
+    width: 30,
+    height: 30,
+  },
+})
