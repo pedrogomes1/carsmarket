131dda632ff3faf32776e59f792cc25e28526474
Pedro Gomes <pedro-gomes@ibm.com>
2023-12-05 14:17:52 -0300

    chore: added fonts to app file

--- App.tsx
+++ App.tsx
@@ -1,7 +1,33 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
-import { Home } from './src/pages/home'
+import { useCallback } from 'react'
+import { SafeAreaView, View } from 'react-native'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+import { Home } from './src/screens/home'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
-  return <Home />
+  const [fontsLoaded] = useFonts({
+    'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-SemiBold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Bold': require('./assets/fonts/Poppins-Bold.ttf'),
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
+
+  if (!fontsLoaded) {
+    return null
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <Home />
+    </SafeAreaView>
+  )
 }
