e9d2d735ec65136e447f658780b7856df900e6da
Pedro Gomes <pedro-gomes@ibm.com>
2023-12-05 14:18:07 -0300

    feat: create static header component

---
+++ src/components/header/header.styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 60,
+    marginRight: 20,
+    marginLeft: 20,
+  },
+  containerProfile: {
+    flexDirection: 'column',
+    marginLeft: 20,
+  },
+  text: {
+    color: '#fff',
+  },
+  username: {
+    fontSize: 16,
+  },
+})
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,20 @@
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
+import { styles } from './header.styles'
+import { Avatar } from '../avatar'
+import { Typography } from '../typography'
 
 export function Header() {
-  return <View>Header</View>
+  return (
+    <View style={styles.container}>
+      <Avatar />
+      <View style={styles.containerProfile}>
+        <Typography text="Welcome," style={styles.text} />
+        <Typography
+          text="Pedro Gomes"
+          style={[styles.text, styles.username]}
+          type="SemiBold"
+        />
+      </View>
+    </View>
+  )
 }
