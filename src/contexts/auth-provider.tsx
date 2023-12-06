a21ae3f8af35ff2b370658f52354b1c75e26b6e3
Pedro Gomes <pedro-gomes@ibm.com>
2023-12-06 18:06:18 -0300

    feat: create auth context and hook to sign in and sign out

---
+++ src/contexts/auth-provider.tsx
@@ -0,0 +1,91 @@
+import { ReactNode, createContext, useEffect, useState } from 'react'
+import * as SecureStore from 'expo-secure-store'
+
+import { useProtectedRouter } from '../hooks/useProtectedRouter'
+import { api } from '../libs/api'
+
+const AUTH_KEY = 'router-manager-token'
+
+type User = {
+  id: string
+  name: string
+  role: string
+  bio: string
+  avatar_url: string
+}
+
+type AuthContextData = {
+  user: User | null
+  signIn: (email: string, password: string) => Promise<void>
+  signOut: () => Promise<void>
+}
+
+export const AuthContext = createContext<AuthContextData>({} as AuthContextData)
+
+type AuthProviderProps = {
+  children: ReactNode
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null)
+  const [userToken, setUserToken] = useState('')
+
+  useProtectedRouter(userToken)
+
+  useEffect(() => {
+    async function loadUser() {
+      const storageToken = await SecureStore.getItemAsync(AUTH_KEY)
+      if (storageToken) {
+        updateToken(storageToken)
+        await getUser()
+      }
+    }
+
+    loadUser()
+  }, [])
+
+  function updateToken(token: string) {
+    setUserToken(token)
+    api.defaults.headers.common.Authorization = `Bearer ${token}`
+  }
+
+  async function getUser() {
+    try {
+      const response = await api.get('/me')
+      const { me } = response.data
+      setUser(me)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async function signIn(email: string, password: string) {
+    try {
+      const response = await api.post('/sessions', {
+        email,
+        password,
+      })
+
+      const { token } = response.data
+
+      updateToken(token)
+      await SecureStore.setItemAsync(AUTH_KEY, token)
+
+      await getUser()
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async function signOut() {
+    await SecureStore.deleteItemAsync(AUTH_KEY)
+    setUserToken('')
+    setUser(null)
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
