00d705bddbe0da792bb01fd8e3a55c940c59359e
Pedro Gomes <pedro-gomes@ibm.com>
2023-12-06 18:07:14 -0300

    chore: added function to logout in header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,14 @@
-import { View } from 'react-native'
-import { styles } from './header.styles'
+import { View, TouchableOpacity } from 'react-native'
+import Icon from '@expo/vector-icons/MaterialIcons'
+
 import { Avatar } from '../Avatar'
 import { Typography } from '../Typography'
+import { useAuth } from '../../hooks/useAuth'
+
+import { styles } from './header.styles'
 
 export function Header() {
+  const { signOut } = useAuth()
   return (
     <View style={styles.container}>
       <Avatar />
@@ -11,6 +16,9 @@ export function Header() {
         <Typography text="Welcome," />
         <Typography text="Pedro Gomes" style={styles.username} type="Bold" />
       </View>
+      <TouchableOpacity onPress={signOut} style={styles.logoutButton}>
+        <Icon name="logout" size={22} color="#fff" />
+      </TouchableOpacity>
     </View>
   )
 }
