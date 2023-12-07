c6be61f6d3b7af5c2c08db1157c286ddee4045a9
Pedro Gomes <pedro-gomes@ibm.com>
2023-12-07 14:32:56 -0300

    feat: create tabs layout

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,38 @@
+import { Tabs } from 'expo-router'
+import Icon from '@expo/vector-icons/AntDesign'
+
+import { colors } from '@/styles/theme'
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveBackgroundColor: colors.darkness,
+        tabBarInactiveBackgroundColor: colors.darkness,
+        tabBarStyle: {
+          borderColor: colors.darkness,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          tabBarIcon: ({ size }) => (
+            <Icon name="home" color={colors.blue_300} size={size} />
+          ),
+          tabBarShowLabel: false,
+        }}
+      />
+      <Tabs.Screen
+        name="register"
+        options={{
+          tabBarIcon: ({ size }) => (
+            <Icon name="plus" color={colors.blue_300} size={size} />
+          ),
+          tabBarShowLabel: false,
+        }}
+      />
+    </Tabs>
+  )
+}
