34cb174f4f1fbc0046ffc483809a8e3935325cfd
Pedro Gomes <pedro-gomes@ibm.com>
2023-12-12 09:09:17 -0300

    chore: added header dynamic username

--- src/components/ui/header/index.tsx
+++ src/components/ui/header/index.tsx
@@ -9,13 +9,17 @@ import { styles } from './header.styles'
 import { colors } from '@/styles/theme'
 
 export function Header() {
-  const { signOut } = useAuth()
+  const { signOut, user } = useAuth()
   return (
     <View style={styles.container}>
       <Avatar />
       <View style={styles.containerProfile}>
         <Typography text="Welcome," />
-        <Typography text="Pedro Gomes" style={styles.username} weight="bold" />
+        <Typography
+          text={user?.name || ''}
+          style={styles.username}
+          weight="bold"
+        />
       </View>
       <TouchableOpacity onPress={signOut} style={styles.logoutButton}>
         <Icon name="logout" size={22} color={colors.white} />
