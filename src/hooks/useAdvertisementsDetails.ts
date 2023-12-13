121e3649e7289622683341cdd873a59e8b4af4bf
Pedro Gomes <pedro-gomes@ibm.com>
2023-12-13 16:52:48 -0300

    feat: create new useAdvertisementsDetails hook

---
+++ src/hooks/useAdvertisementsDetails.ts
@@ -0,0 +1,40 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { api } from '@/libs/api'
+
+export interface Advertisement {
+  id: string
+  picture: string
+  description: string
+  model: string
+  year: string
+  value: number
+  city: string
+  brand: {
+    id: string
+    logo: string
+  }
+  favorites: [
+    {
+      id: string
+      isFavorite: boolean
+    },
+  ]
+}
+export interface Advertisements {
+  advertisements: Advertisement
+}
+
+async function fetchAdvertisementsDetails(id: string): Promise<Advertisements> {
+  const response = await api.get(`/advertisements/${id}`)
+  return response.data
+}
+
+export function useAdvertisementsDetails(id: string) {
+  const { data, isError, isPending } = useQuery({
+    queryKey: ['advertisementsList'],
+    queryFn: () => fetchAdvertisementsDetails(id),
+  })
+
+  return { data: data?.advertisements, isPending, isError }
+}
