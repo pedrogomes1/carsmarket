f3275361789c664907eb1d3918918a52738d8119
Pedro Gomes <pedro-gomes@ibm.com>
2023-12-26 10:20:43 -0300

    chore: remove firebase credentials

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,12 +3,12 @@ import { getStorage } from 'firebase/storage'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCTqR3Mk9joU5iJk1OpZSX9uswfHeGzsh8',
-  authDomain: 'carsmarket-695af.firebaseapp.com',
-  projectId: 'carsmarket-695af',
-  storageBucket: 'carsmarket-695af.appspot.com',
-  messagingSenderId: '785397015372',
-  appId: '1:785397015372:web:b47be2de421b3a411965e9',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 }
 
 const app = initializeApp(firebaseConfig)
